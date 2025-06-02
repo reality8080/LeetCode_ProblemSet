a0677c2bfbc4d3f8c03a0e6769ac6789960fe045
reality8080 <155755865+reality8080@users.noreply.github.com>
2025-06-02 20:58:27 +0700

    Solve Problem by 2 For Combine OR Hash  Table

---
+++ Test/TwoNumber.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i] + nums[j] == target){
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // Trả về vector rỗng nếu không tìm thấy
+    }
+
+    vector<int> twoSumChatBot(vector<int>& nums, int target) {
+        unordered_map<int,int> unorderedMap;
+        for(int i=0;i<nums.size();i++){
+            int difference = target - nums[i];
+            // Kiểm tra xem difference đã có trong unorderedMap chưa
+            if(unorderedMap.find(difference)!=unorderedMap.end()){
+                // Nếu có, trả về chỉ số của difference và chỉ số hiện tại
+                return {unorderedMap[difference], i};
+            }
+            // Nums[i] là giá trị của i trong mảng được đưa vào băm làm key, i là chỉ số trong mảng 1 chiều
+            unorderedMap[nums[i]] = i;
+        }
+        return {}; 
+    }
+
+};
+
+int main(){
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = sol.twoSum(nums, target);
+    if(!result.empty()){
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    }else{
+        cout << "No two sum solution found." << endl;
+    }
+    return 0;
+}
